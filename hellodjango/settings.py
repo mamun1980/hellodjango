3c1a1e412c70659b026ea711e487890dd822a743
K Md Mamunur Rashid <mamun1980@gmail.com>
2014-10-02 02:40:36 +0600

    Change db settings

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -48,16 +48,20 @@ WSGI_APPLICATION = 'hellodjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES['default'] =  dj_database_url.config()
+# DATABASES['default'] =  dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'test',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd8ps687cfa1r3j',
+        'PASSWORD': 'IFPv6wVk8z-H4xJ8yyNO9MTNfN',
+        'HOST': 'ec2-23-23-81-171.compute-1.amazonaws.com',
+        'PORT': '5432',
+
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
